9abd5b821ca50aa4eddec8e1fecc23988dc0bfa0
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-27 12:10:35 +0100

    create main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/*Scrivere una funzione che controlli se un numero int è in un 
+determinato range. Se lo, è ritorna true ; se non lo è, lancia 
+un'eccezione.*/
+
+public class Main {
+    public static void main (String[] args){
+
+    }
+}
