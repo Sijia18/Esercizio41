9e8d2cab17f5d29384ff3091aa160a6c606641e6
Aurora Scalici <aurorascalici1899@icloud.com>
2024-03-06 14:51:27 +0100

    verified

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,24 @@ un'eccezione.*/
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            int number = 11;
-            int minimum = 0;
-            int maximum = 10;
-
-            if (isInRange(number, minimum, maximum)) {
-                System.out.println("The number is in range!");
-            }
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-            // getMessage() serve a nascondere la dicitura java.lang.ArithmeticException:
+        int number = 11;
+        int minimum = 0;
+        int maximum = 10;
+        if (isInRange(number, minimum, maximum)) {
+            System.out.println("The number is in range!");
         }
     }
 
     public static boolean isInRange(int number, int minimum, int maximum) {
-        if (number < minimum || number > maximum) {
-            throw new ArithmeticException("The number is out of bounds.\nTry with a number between 0 and 10!");
+        try {
+            if (number < minimum || number > maximum) {
+                throw new ArithmeticException("The number is out of bounds.\nTry with a number between 0 and 10!");
+            }
+            return true;
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+            // getMessage() serve a nascondere la dicitura java.lang.ArithmeticException:
         }
-        return true;
+        return false;
     }
 }
